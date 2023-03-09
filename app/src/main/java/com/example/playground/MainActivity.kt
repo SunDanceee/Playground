41bd20a28d47b5d970defd3f7d4a764febfb84d4
cnie <cnie@microstrategy.com>
2023-03-09 09:36:26 +0800

    set up compose

--- app/src/main/java/com/example/playground/MainActivity.kt
+++ app/src/main/java/com/example/playground/MainActivity.kt
@@ -1,13 +1,20 @@
 package com.example.playground
 
 import android.os.Bundle
+import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.material.MaterialTheme
+import com.example.home.ui.HomeScreen
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            MaterialTheme {
+                HomeScreen()
+            }
+        }
     }
 }
---
+++ feature/home/src/main/java/com/example/home/ui/HomeScreen.kt
@@ -0,0 +1,9 @@
+package com.example.home.ui
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun HomeScreen() {
+    Text(text = "Home")
+}
